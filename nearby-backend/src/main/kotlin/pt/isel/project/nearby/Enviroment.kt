b341cc1c1adb597c3b25f93cece459667fcb498d
Manuel Santos <a49459@alunos.isel.pt>
2025-06-25 12:44:23 +0100

    Changed the name of Enviroment

--- nearby-backend/src/main/kotlin/pt/isel/project/nearby/Enviroment.kt
+++ nearby-backend/src/main/kotlin/pt/isel/project/nearby/Enviroment.kt
@@ -3,7 +3,7 @@ package pt.isel.project.nearby
 object Environment {
     fun getDbUrl() = System.getenv(KEY_DB_URL) ?: throw RuntimeException("Environment variable $KEY_DB_URL not found")
     fun getDbUser() = "postgres"
-    fun getDbPassword() = "Ricardo"
+    fun getDbPassword() = "manuecana"
     private const val KEY_DB_URL = "KEY_DB_URL"
 
 }
