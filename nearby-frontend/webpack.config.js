549d1735092ea46ecdb5d9c07e89b31338664394
Manuel Santos <a49459@alunos.isel.pt>
2025-06-26 15:19:39 +0100

    Commit the Docker

--- nearby-frontend/webpack.config.js
+++ nearby-frontend/webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -65,15 +63,5 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'public', to: '.', globOptions: { ignore: ['**/index.html'] } },
-            ],
-        }),
-    ],
+    }
 };
